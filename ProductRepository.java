43fcb64702ea794793792ecd2b517f7ae05c5390
Carbon footprint <pavanbalaji545@gmail.com>
2026-01-21 19:49:10 +0530

    Add files via upload

---
+++ ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.Entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Basic CRUD is enough for now
+}
